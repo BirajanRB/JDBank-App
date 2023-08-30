299bd1018274f7ecd3d0aadc418129ec6a6bf8de
birajan <birajanrb@gmail.com>
2023-08-31 05:18:59 +0545

    update on project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
-import Homepage from "./Components/Homepage/Homepage.jsx";
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+import LandingPage from "./Homepage/LandingPage";
+import MeetOurTeamPage from "./ManagmentTeamPage/MeetOurTeamPage";
+import AboutUsPage from "./AboutUspage/AboutUsPage";
+import RegistrationPage from "./Registration/RegistrationPage";
+import LoginPageNewVersion from "./LoginPage/LoginPageNewVersion";
+import LoginPage from "./Login/pages/LoginPage";
+
+export default function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/Services" element={"Services"} />
-        <Route path="/ManagementTeam" element={"ManagementTeam"} />
-        <Route path="/About" element={"About"} />
-        <Route path="/CustomerSupport" element={"CustomerSupport"} />
-      </Routes>
-    </>
+    <div className="App">
+      <>
+        <Routes>
+          <Route path="/" element={<LandingPage />}>
+            <Route path="/Login" element={<LoginPageNewVersion />} />
+          </Route>
+
+          <Route path="/ManagementTeam" element={<MeetOurTeamPage />}>
+            <Route
+              path="/ManagementTeam/Login"
+              element={<LoginPageNewVersion />}
+            />
+          </Route>
+
+          <Route path="/AboutUsPage" element={<AboutUsPage />}>
+            <Route
+              path="/AboutUsPage/Login"
+              element={<LoginPageNewVersion />}
+            />
+          </Route>
+
+          <Route path="/Registration" element={<RegistrationPage />} />
+
+          <Route path="/LoginDefault" element={<LoginPage />} />
+
+          <Route path="*" element={<h1>PageNotFound! :(</h1>} />
+        </Routes>
+      </>
+    </div>
   );
 }
-
-export default App;
